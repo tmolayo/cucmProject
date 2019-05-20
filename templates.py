6975227e3e61e7d92b2fabbae73c43e92f306466
DanielCarmel <danielcarmel6@gmail.com>
2019-05-20 16:13:17 +0300

    Template functionality works in a scary wayyy

--- templates.py
+++ templates.py
@@ -1,80 +1,31 @@
 import json
-from string import Template
-
-#with open('config.json', 'r') as file:
-#    config = json.load(file)
+from jinja2 import Template
 
 
-class Phone:
-    def __init__(self, name):
-        with open('./configuration/phone.json', 'r') as file:
+class GeneralTemplate:
+    def __init__(self, path, **kwargs):
+        with open(path) as file:
             config = json.load(file)
+
+        if kwargs is not None:
             config = Template(json.dumps(config))
-            config = config.safe_substitute(name=name)
-        print(json.loads(config))
-        self.attributes = config
-        #self.attributes['name'] = name
-
-    def __get__(self, instance, owner):
-        return self.attributes
-
-a = Phone(name='didid')
-print(a.attributes)
+            config = config.render(**kwargs)
+            self.attributes = json.loads(config)
 
 
+class Phone(GeneralTemplate):
+    def __init__(self, path='./configuration/phone.json', **kwargs):
+        GeneralTemplate.__init__(self, path=path, **kwargs)
 
 
-class Phone1:
-
-    def __init__(self, name):
-        #self.attributes = config['COMPONENTS']['phone']
-        self.attributes['name'] = name
-
-    def __set__(self, instance, name):
-        self.attributes['name'] = name
-
-    def __delete__(self, instance):
-        del self.attributes
-
-    def __get__(self, instance, owner):
-        return self.attributes
+class Line(GeneralTemplate):
+    def __init__(self, path='./configuration/line.json', **kwargs):
+        GeneralTemplate.__init__(self, path=path, **kwargs)
 
 
-class Line:
-    def __init__(self, lineId):
-        lineId = str(lineId)
-        #self.attributes = config['COMPONENTS']['line']
-        self.attributes["pattern"] = lineId
-        self.attributes["description"] = "description " + lineId
-        self.attributes["alertingName"] = "alertingName " + lineId
-
-    def __set__(self, instance, lineId):
-        lineId = str(lineId)
-        self.attributes["pattern"] = lineId
-        self.attributes["description"] = "description " + lineId
-        self.attributes["alertingName"] = "alertingName " + lineId
-
-    def __get__(self, instance, owner):
-        return self.attributes
-
-    def __delete__(self, instance):
-        del self.attributes
+class PhoneWithLine(GeneralTemplate):
+    def __init__(self, path='./configuration/phoneWithLine.json', **kwargs):
+        GeneralTemplate.__init__(self, path=path, **kwargs)
 
 
-class PhoneWithLine:
-    def __init__(self, phoneName, lineUuid, lineId):
-        #self.attributes = config['COMPONENTS']['phoneWithLine']
-        self.attributes['name'] = 'CSF' + phoneName
-        self.attributes['lines']['line']['dirn']['uuid'] = lineUuid
-        self.attributes['lines']['line']['dirn']['pattern'] = lineId
-
-    def __set__(self, instance, phoneName, lineUuid, lineId):
-        self.attributes['name'] = 'CSF' + phoneName
-        self.attributes['lines']['line']['dirn']['uuid'] = lineUuid
-        self.attributes['lines']['line']['dirn']['pattern'] = lineId
-
-    def __get__(self, instance, owner):
-        return self.attributes
-
-    def __delete__(self, instance):
-        del self.attributes
+# print(PhoneWithLine(name='dani123', id=8129664, uuid=12323123).attributes)
