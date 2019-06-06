5b2232dd6fbc15b14bb8963e52d3259471c6d02c
DanielCarmel <danielcarmel6@gmail.com>
2019-06-06 12:48:44 +0300

    finished functionality of LDAP calls

--- ldapCalls.py
+++ ldapCalls.py
@@ -7,26 +7,27 @@ class LdapConnection:
         self.connection = Connection(server, user=username, password=password)
         self.connection.bind()
 
-    @staticmethod
-    def get_user_ipphone(user_name, connection):
-        connection.bind()
-        connection.search(search_base='search base', search_filter='(samAccountName=' + user_name + ')',
-                          attributes=['cn', 'samAccountName', 'ipPhone'])
-        ip_phone = connection.response[0].get('attributes').get('ipPhone')
-        return ip_phone
+    def get_user_ip_phone(self, user_name):
+        self.connection.bind()
+        self.connection.search(search_base='search base', search_filter='(samAccountName=' + user_name + ')',
+                               attributes=['cn', 'samAccountName', 'ipPhone'])
+        ip_phone = self.connection.response[0].get('attributes').get('ipPhone')
+        if not ip_phone:
+            return None
+        else:
+            return ip_phone
 
-    @staticmethod
-    def get_user_dn(user_name, connection):
-        connection.search(search_base='search base', search_filter='(samAccountName=' + user_name + ')',
-                          attributes=['cn', 'samAccountName', 'ipPhone'])
-        return connection.response[0].get('dn')
+    def get_user_dn(self, user_name):
+        self.connection.search(search_base='search base', search_filter='(samAccountName=' + user_name + ')',
+                               attributes=['cn', 'samAccountName', 'ipPhone'])
+        return self.connection.response[0].get('dn')
 
-    def set_user_ip_phone(self, user_name, ip_phone, connection):
-        connection.bind()
-        user_dn = self.getUserDN(user_name, connection)
-        is_modified = connection.modify(dn=user_dn, changes={'ipPhone': [(MODIFY_REPLACE, [str(ip_phone)])]})
+    def set_user_ip_phone(self, user_name, ip_phone):
+        self.connection.bind()
+        user_dn = self.getUserDN(user_name, self.connection)
+        is_modified = self.connection.modify(dn=user_dn, changes={'ipPhone': [(MODIFY_REPLACE, [str(ip_phone)])]})
         return is_modified
 
-    def end_connection(self):
+    def __del__(self):
         self.connection.unbind()
         del self.connection
